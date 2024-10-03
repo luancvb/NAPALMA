91168be67b20ebd04f36493089295ac28fb21ae8
luancvb <luancaiquelcvb@gmail.com>
2024-10-02 22:18:23 -0300

    Botões

--- pagina_principal/pagina_de_marcacao/paginas_servicos/consultas/script-menu.js
+++ pagina_principal/pagina_de_marcacao/paginas_servicos/consultas/script-menu.js
@@ -1,3 +1,7 @@
 function navigateTo(page) {
     window.location.href = "consultas.html";
 }
+
+function navigateToAgendadas(page) {
+    window.location.href = "consultas-agendadas/agendadas.html";
+}
